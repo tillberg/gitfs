7bd5915104b08450bcb48178141ffb0d750a6f34
Dan Tillberg <dan@sky.(none)>
2011-11-20 14:40:14 -0500

    readFile

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-
+require('tillberg_common');
 var fs = require('fs');
 var vows = require('vows');
 var assert = require('assert');
@@ -124,6 +124,12 @@ repoFuncTests.addBatch({
 });
 repoFuncTests.export(module);
 
+// Remove the old gitfs-var folder.  Do this kind of sloppily so
+// that the `Basic Usage` tests can just use gitfs without going
+// through the `getGit()` guard like the code above.
+try { fs.renameSync('gitfs-var', 'gitfs-destroy'); } catch (ex) { }
+exec('rm', ['-rf', 'gitfs-destroy'], { cwd: './' });
+
 var basicUsageTests = vows.describe('Basic Usage');
 basicUsageTests.addBatch({
   'If we load a file from a repo': {
@@ -131,7 +137,53 @@ basicUsageTests.addBatch({
       gitfs.repo('git://github.com/tillberg/euler.git').readFile('README', this.callback);
     },
     'We should get the contents': function(err, data) {
+      assert.ifError(err);
       assert.match(data, /Project Euler in Ruby and Scala/);
+    }
+  },
+  'We can get the SHA1 of a branch': {
+    topic: function() {
+      gitfs.repo('git://github.com/tillberg/euler.git').branch('master', this.callback);
+    },
+    'and it is correct': function(err, tree) {
+      assert.ifError(err);
+      assert.equal('62220098cf3a628110022f770fe8af874f547d4a', tree.sha1);
+    }
+  },
+  'We can checkout a snapshot of a repo to a local path': {
+    topic: function() {
+      gitfs.repo('git://github.com/tillberg/euler.git').checkout('var/euler', this.callback);
+    },
+    'and it doesn\'t error out': function(err, d) {
+      assert.ifError(err);
+    },
+    'then load a file from the local path': {
+      topic: function() {
+        fs.readFile('var/euler/euler.rb', 'utf8', this.callback);
+      },
+      'and the file should contain a method to find the sum of weird numbers': function(err, data) {
+        assert.ifError(err);
+        assert.match(data, /nums\.find_all\{\|n\| n\.is_weird\(maxd\)\}\.sum/);
+      }
+    },
+    'then do a find on the checkout path': {
+      topic: function() {
+        exec('find', [], { cwd: 'var/euler/' }, this.callback);
+      },
+      'and we should find 17 items': function(err, data) {
+        assert.ifError(err);
+        // Specifically, we don't want a copy of any git-specific stuff
+        assert.lengthOf(_.trim(data).split('\n'), 17);
+      }
+    }
+  },
+  'We can get a file from a specific version of a repo': {
+    topic: function() {
+      gitfs.repo('git://github.com/tillberg/euler.git').tree('e9aa251ff406451af7775cb14ec3d878e4afac61').readFile('euler.rb', this.callback);
+    },
+    'and it should have the contents as of that version': function(err, data) {
+      assert.match(data, /class Array\; def sum/);
+      assert.lengthOf(data, 885);
     }
   }
 });
